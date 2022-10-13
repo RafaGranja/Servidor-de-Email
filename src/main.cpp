d8aebc32904783e0d6abaa423f97d20e35ba2dcf
Rafael Granja <rafaelgranja2003@gmail.com>
2022-10-13 00:31:15 -0300

    Insert na lista

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Email{
 
+    public :
     int pri;
     std::string msg;
 
@@ -11,16 +12,18 @@ class Email{
 
 class ListaEmail{
 
-    Email data;
-    ListaEmail *next;
+    public :
+    Email *data=NULL;
+    ListaEmail *next=NULL;
 
 };
 
 class CaixaDeEntrada{
 
+    public :
     ListaEmail *listaEmail;
 
-    void push(Email new_email){
+    void push(Email *new_email){
 
         /* 1. create and allocate node */
         ListaEmail* newNode = new ListaEmail;
@@ -28,76 +31,35 @@ class CaixaDeEntrada{
         /* 2. assign data to node */
         newNode->data = new_email;
         
-        if(this->data)
-
-    }
-        
-        //insert new node after a given node
-    void insertAfter(ListaEmail* prev_node, Email node_data){
-
-        /*1. check if the given prev_node is NULL */
-        if (prev_node == NULL)
-        {
-        cout<<"the given previous node is required,cannot be NULL"; return; } 
-        
-        /* 2. create and allocate new node */
-        ListaEmail* newNode =new ListaEmail; 
-        
-        /* 3. assign data to the node */
-        newNode->data = node_data;
-        
-        /* 4. Make next of new node as next of prev_node */
-        newNode->next = prev_node->next;
-        
-        /* 5. move the next of prev_node as new_node */
-        prev_node->next = newNode;
-
-    }
-        
-        /* insert new node at the end of the linked list */
-    void append(ListaEmail** head, Email node_data){
-
-        /* 1. create and allocate node */
-        ListaEmail* newNode = new ListaEmail;
-        
-        ListaEmail *last = *head; /* used in step 5*/
-        
-        /* 2. assign data to the node */
-        newNode->data = node_data;
-        
-        /* 3. set next pointer of new node to null as its the last node*/
-        newNode->next = NULL;
-        
-        /* 4. if list is empty, new node becomes first node */
-        if (*head == NULL)
-        {
-            *head = newNode;
-            return;
+        if(this->listaEmail == NULL){
+            this->listaEmail = newNode;
         }
-        
-        /* 5. Else traverse till the last node */
-        while (last->next != NULL)
-            last = last->next;
-        
-        /* 6. Change the next of last node */
-        last->next = newNode;
-        return;
+        else{
+            ListaEmail* aux = new ListaEmail;
+            ListaEmail* Node = this->listaEmail;
+            Node = this->listaEmail;
+            if(Node->data->pri<newNode->data->pri){
 
-    }
-        
-        // display linked list contents
-    // void displayList(ListaEmail *node){
+                aux=this->listaEmail;
+                newNode->next=aux;
+                this->listaEmail = newNode;
+                delete aux;
 
-    //     //traverse the list to display each node
-    //     while (node != NULL)
-    //     {
-    //         cout<<node->data<<"-->";
-    //         node = node->next;
-    //     }
-        
-    //     if(node== NULL)
-    //     cout<<"null"; 
-    // } 
+            }
+            else{
+                while (Node->next->data->pri>=newNode->data->pri || Node->next==NULL)
+                {
+                    Node=Node->next;
+                }
+                aux=Node->next;
+                newNode->next=aux;
+                Node->next = newNode;
+                delete aux;
+            }
+            
+        }
+
+    } 
 
 };
 
