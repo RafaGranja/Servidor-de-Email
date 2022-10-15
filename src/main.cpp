54c6d4801c3409ef6f0e5346416140da8fa88c9b
Rafael Granja <rafaelgranja2003@gmail.com>
2022-10-15 17:55:52 -0300

    Correção de leitura de dados e adição de comentários no main

--- src/main.cpp
+++ src/main.cpp
@@ -11,38 +11,31 @@ int main(){
     unsigned int id;
     unsigned int pri;
     string msg;
-    string aux;
 
     string c;
 
-    while(cin >> c){
+    while(cin >> c){//ENQUANTO HOUVER INPUTS
 
-        for (auto & a: c) a = toupper(a);
-        if(c=="CADASTRA"){
-            cin >> id;
-            usuarios->push(id);
+        for (auto & a: c) a = toupper(a);//TRANSFORMA COMANDO PARA CAIXA ALTA
+        if(c=="CADASTRA"){//SE OPERACAO DE CADASTRO DE USUARIO
+            cin >> id;//LE O ID DO USUARIO
+            usuarios->push(id);//CAHAMA FUNCAO DE CADASTRO PASSANDO O ID
         }
-        else if(c=="REMOVE"){
-            cin >> id;
-            usuarios->pop(id);
+        else if(c=="REMOVE"){//SE OPERACAO DE EXCLUIR USUARIO
+            cin >> id;//LE O ID DO USUARIO
+            usuarios->pop(id);//CHAMA A FUNCAO DE EXCLUSAO PASSANDO O ID
 
         }
-        else if(c=="ENTREGA"){
-            cin >> id;
-            cin >> pri;
-            // cin >> aux;
-            // while (aux !="FIM")
-            // {
-            //     msg+=aux+" ";
-            //     cin >> aux;
-            // }
-            getline(std::cin,msg);
-            msg = msg.substr(0,(msg.length() - 3));
-            usuarios->push_email(msg,pri,id);
+        else if(c=="ENTREGA"){//SE OPERACAO DE ENVIAR EMAIL
+            cin >> id;//LE O ID DO USUARIO
+            cin >> pri;//LE A PRIORIDADE
+            getline(std::cin>>ws,msg);//LE TODA A MENSAGEM QUE SERÁ ENVIADA
+            msg = msg.substr(0,(msg.length() - 3));//RETIRA O 'FIM'
+            usuarios->push_email(msg,pri,id);//CHAMA FUNCAO DE ENVIO DE EMAIL PASSANDO A MENSAGEM, PRIORIDADE DA MENSAGEM E ID DO USUARIO
         }
-        else if(c=="CONSULTA"){
-            cin >> id;
-            usuarios->pop_email(id);
+        else if(c=="CONSULTA"){//SE OPERACAO DE LER EMAIL
+            cin >> id;//LE O ID DO USUARIO
+            usuarios->pop_email(id);//CHAMA FUNCAO DE LEITURA DE EMAIL PASSANDO O ID DO USUARIO
         }
 
     }
